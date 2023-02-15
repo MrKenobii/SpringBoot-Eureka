4725ece30e708c43b0af82efed7d13a2d460c0f9
MrKenobii <anldygc35@windowslive.com>
2023-02-15 14:37:15 +0300

    Spring Security with Okta Config

--- hotel-service/src/main/java/com/kenobi/hotelservice/controllers/HotelController.java
+++ hotel-service/src/main/java/com/kenobi/hotelservice/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.kenobi.hotelservice.services.HotelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,14 +16,17 @@ import java.util.List;
 public class HotelController {
     private final HotelService hotelService;
 
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.createHotel(hotel));
     }
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Hotel>> getAllHotels(){
         return ResponseEntity.ok(hotelService.getAllHotels());
     }
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> getHotelById(@PathVariable String hotelId){
         return ResponseEntity.ok(hotelService.getHotel(hotelId));
---
+++ user-service/src/main/java/com/kenobi/userservice/config/interceptor/FeignInterceptor.java
@@ -0,0 +1,18 @@
+package com.kenobi.userservice.config.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class FeignInterceptor implements RequestInterceptor {
+    private OAuth2AuthorizedClientManager manager;
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        String token = manager.authorize(OAuth2AuthorizeRequest.withClientRegistrationId("my-internal-client").principal("internal").build()).getAccessToken().getTokenValue();
+        requestTemplate.header("Authorization", "Bearer " + token);
+    }
+}
